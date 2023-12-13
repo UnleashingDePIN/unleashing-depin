36380c2e7fdc8fab96da14f47e67979c22a64f91
meta-lite <nick@myceliumnetworks.com>
2023-12-13 13:51:34 -0600

    add episode links

--- src/app/page.js
+++ src/app/page.js
@@ -14,78 +14,78 @@ export default function Home(props) {
     mobile: { breakpoint: { max: 464, min: 0 }, items: 1, slidesToSlide: 1 }
   };
   const episodes = {
-    // ep1: {
-    //   image: "Thumbnails/ep1.png",
-    //   youtube: "https://www.youtube.com/@unleashingdepin",
-    //   spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin",
-    //   apple: "https://podcasts.apple.com/us/podcast/unleashing-depin/id1643813996"
-    // },
-    // ep2: {
-    //   image: "Thumbnails/ep2.png",
-    //   youtube: "https://www.youtube.com/@unleashingdepin",
-    //   spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin",
-    //   apple: "https://podcasts.apple.com/us/podcast/unleashing-depin/id1643813996"
-    // },
-    // ep3: {
-    //   image: "Thumbnails/ep3.png",
-    //   youtube: "https://www.youtube.com/@unleashingdepin",
-    //   spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin",
-    //   apple: "https://podcasts.apple.com/us/podcast/unleashing-depin/id1643813996"
-    // },
-    // ep4: {
-    //   image: "Thumbnails/ep4.png",
-    //   youtube: "https://www.youtube.com/@unleashingdepin",
-    //   spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin",
-    //   apple: "https://podcasts.apple.com/us/podcast/unleashing-depin/id1643813996"
-    // },
-    // ep5: {
-    //   image: "Thumbnails/ep5.png",
-    //   youtube: "https://www.youtube.com/@unleashingdepin",
-    //   spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin",
-    //   apple: "https://podcasts.apple.com/us/podcast/unleashing-depin/id1643813996"
-    // },
+    ep1: {
+      image: "Thumbnails/ep1.png",
+      youtube: "https://www.youtube.com/watch?v=FAUD0h5ZkWE",
+      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin/episodes/Unleashing-DeWi-001---A-Conversation-with-LongFi-Solutions-CEO--Josh-Heller-e1nkdbb",
+      apple: "https://podcasts.apple.com/us/podcast/unleashing-dewi-001-a-conversation-with-longfi/id1643813996?i=1000579005834"
+    },
+    ep2: {
+      image: "Thumbnails/ep2.png",
+      youtube: "https://www.youtube.com/watch?v=s2_qVViiIsA",
+      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin/episodes/Unleashing-DeWi-002--Investing-In-DeWi-with-David-Byrd-of-BlueYard-Capital-e1r5bce",
+      apple: "https://podcasts.apple.com/us/podcast/unleashing-dewi-002-investing-in-dewi-with-david-byrd/id1643813996?i=1000587158853"
+    },
+    ep3: {
+      image: "Thumbnails/ep3.png",
+      youtube: "https://www.youtube.com/watch?v=BsbdDRzQL3g",
+      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin/episodes/Unleashing-DeWi-003--Building-DeWi-Picks-and-Shovels-with-Andrew-Fisher-of-SolSplits-e1sld7i",
+      apple: "https://podcasts.apple.com/us/podcast/unleashing-dewi-003-building-dewi-picks-and-shovels/id1643813996?i=1000591156272"
+    },
+    ep4: {
+      image: "Thumbnails/ep4.png",
+      youtube: "https://www.youtube.com/watch?v=3XhDKfEa1Rk",
+      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin/episodes/Unleashing-DeWi-004---A-Conversation-with-Richard-DeVaul--CEO-of-XNET-e1v1gq4",
+      apple: "https://podcasts.apple.com/us/podcast/unleashing-dewi-004-a-conversation-with-richard/id1643813996?i=1000599857346"
+    },
+    ep5: {
+      image: "Thumbnails/ep5.png",
+      youtube: "https://www.youtube.com/watch?v=vR02En_cTVI",
+      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin/episodes/Unleashing-DeWi-005---Unlocking-the-Business-Value-of-Heliums-LoRaWAN-Network-With-Miroslav--CEO-of-Heliotics-e20kus1",
+      apple: "https://podcasts.apple.com/us/podcast/unleashing-dewi-005-unlocking-the-business-value/id1643813996?i=1000604713962"
+    },
     ep6: {
       image: "Thumbnails/ep6.png",
-      youtube: "https://www.youtube.com/@unleashingdepin",
-      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin",
-      apple: "https://podcasts.apple.com/us/podcast/unleashing-depin/id1643813996"
+      youtube: "https://www.youtube.com/watch?v=14OJKJnMn_c",
+      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin/episodes/Unleashing-DeWi-006---Real-World-Data-For-Proof-of-Efficiency-with-enviroBLOQ-e26b1g8",
+      apple: "https://podcasts.apple.com/us/podcast/unleashing-dewi-006-real-world-data-for-proof-of/id1643813996?i=1000618705346"
     },
     ep7: {
       image: "Thumbnails/ep7.png",
-      youtube: "https://www.youtube.com/@unleashingdepin",
-      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin",
-      apple: "https://podcasts.apple.com/us/podcast/unleashing-depin/id1643813996"
+      youtube: "https://www.youtube.com/watch?v=F6YZkS65rKo",
+      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin/episodes/Unleashing-DePIN-007---Scaling-DePIN-With-Eclipse---A-Conversation-With-Veronica-Buron-e280qm3",
+      apple: "https://podcasts.apple.com/us/podcast/unleashing-depin-007-scaling-depin-with-eclipse/id1643813996?i=1000624206656"
     },
     ep8: {
       image: "Thumbnails/ep8.png",
-      youtube: "https://www.youtube.com/@unleashingdepin",
-      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin",
-      apple: "https://podcasts.apple.com/us/podcast/unleashing-depin/id1643813996"
+      youtube: "https://www.youtube.com/watch?v=XibhJjaSV6g",
+      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin/episodes/Unleashing-DePIN-008---A-Conversation-With-Elijah-From-Relay-Wireless-e2ar4m4",
+      apple: "https://podcasts.apple.com/us/podcast/unleashing-depin-008-a-conversation-with-elijah/id1643813996?i=1000632025109"
     },
     ep9: {
       image: "Thumbnails/ep9.png",
-      youtube: "https://www.youtube.com/@unleashingdepin",
-      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin",
-      apple: "https://podcasts.apple.com/us/podcast/unleashing-depin/id1643813996"
+      youtube: "https://www.youtube.com/watch?v=EduGR07SoDc",
+      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin/episodes/Unleashing-DePIN-009---Chatting-With-Alex-Rawitz-the-Co-Founder-of-DIMO-e2ar4oc",
+      apple: "https://podcasts.apple.com/us/podcast/unleashing-depin-009-chatting-with-alex-rawitz-the/id1643813996?i=1000632027849"
     },
     ep10: {
       image: "Thumbnails/ep10.png",
-      youtube: "https://www.youtube.com/@unleashingdepin",
-      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin",
-      apple: "https://podcasts.apple.com/us/podcast/unleashing-depin/id1643813996"
+      youtube: "https://www.youtube.com/watch?v=7BRezDfQ1fE",
+      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin/episodes/Unleashing-DePIN-010---A-Conversation-With-Gabe-From-Hivemapper-e2bevl5",
+      apple: "https://podcasts.apple.com/us/podcast/unleashing-depin-010-a-conversation-with-gabe/id1643813996?i=1000633640584"
     },
     // ep11: {
     //   image: "Thumbnails/ep11.png",
-    //   youtube: "https://www.youtube.com/@unleashingdepin",
-    //   spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin",
-    //   apple: "https://podcasts.apple.com/us/podcast/unleashing-depin/id1643813996"
+    //   youtube: "https://www.youtube.com/watch?v=JoN6uzHvnVg",
+    //   spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin/episodes/Unleashing-DePIN-011---DePIN-Powered-Noise-Pollution-Mapping-With-Silencio-Network-e2chku9",
+    //   apple: "https://podcasts.apple.com/us/podcast/unleashing-depin-011-depin-powered-noise-pollution/id1643813996?i=1000636850604"
     // },
-    // ep12: {
-    //   image: "Thumbnails/ep12.png",
-    //   youtube: "https://www.youtube.com/@unleashingdepin",
-    //   spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin",
-    //   apple: "https://podcasts.apple.com/us/podcast/unleashing-depin/id1643813996"
-    // },
+    ep12: {
+      image: "Thumbnails/ep12.png",
+      youtube: "https://www.youtube.com/watch?v=UgdkuFpbk4c",
+      spotify: "https://podcasters.spotify.com/pod/show/unleashing-depin/episodes/Unleashing-DePIN-012---The-Mycelium-Testbed---A-DePIN-Utility---With-Rishi-Mittal-e2cospj",
+      apple: "https://podcasts.apple.com/us/podcast/unleashing-depin-012-the-mycelium-testbed-a/id1643813996?i=1000637364497"
+    },
   }
   const episodesArray = Object.values(episodes);
   return (
