1b90d75569ea4b190d4b18b66db6f7b800f46eda
tylerboscolo <tyler@boscolo.net>
2024-01-18 11:25:57 -0600

    fix typeform mobile

--- src/app/components/TypeformEmbed.js
+++ src/app/components/TypeformEmbed.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Widget } from '@typeform/embed-react'
 
 const TypeformEmbed = () => {
-  return <Widget id="https://form.typeform.com/to/vPh0EEH7" style={{ width: '100%', height: '35vh' }} className="my-form" />
+  return <Widget id="https://form.typeform.com/to/vPh0EEH7?typeform-welcome=0" style={{ width: '100%', height: '35vh' }} className="my-form" />
 }
 
 export default TypeformEmbed;
