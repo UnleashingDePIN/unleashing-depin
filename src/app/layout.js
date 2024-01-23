a8f660ff4c33d2bf23dbc4b17d7b18714c0dabda
Tyler Boscolo <31681660+tylerboscolo@users.noreply.github.com>
2024-01-23 16:32:00 -0600

    Update layout.js
    GSC verification tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" /></head>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+        <meta name="google-site-verification" content="pGYeQXTsutqLCDxQqrPZOxUHGN84zT9979eyuKxSejQ" />
+      </head>
       <body className={inter.className}>
         {children} 
         <Analytics />
