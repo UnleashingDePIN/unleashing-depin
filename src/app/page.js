66ad95b8e12ff430174014e0588ee0d2acbe8231
meta-lite <nick@myceliumnetworks.com>
2024-02-27 09:26:58 -0600

    add team componet

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,7 @@ import TypeformEmbed from './components/TypeformEmbed';
 import './styles.css';
 import episodes from './episodes.js';
 import Header from './components/header.js';
+import Team from './components/team.js';
 
 export default function Home(props) {
   const responsive = {
@@ -105,25 +106,7 @@ export default function Home(props) {
           </div>
         </Carousel>;
       </div>
-      <div className="find-us-section"><h2>Our Team</h2></div>
-      <div className="team-section">
-        <a className="team-link" style={{ padding: '0,20%' }} >
-          <img src="tyler.jpg" alt="Tyler Boscolo" width="300" height="300" style={{borderRadius: '10px'}}/>
-          <div className="team-text"><h2>Tyler Boscolo</h2></div>
-          <div className="team-buttons">
-            <a href="https://www.linkedin.com/in/tylerboscolo" className="other-icon"><img src="/linkedin.png" alt="LinkedIn" height="100%"/></a>
-            <a href="https://twitter.com/tylerboscolo" className="other-icon"><img src="x.png" alt="X Logo" height="100%" /></a>
-          </div>
-        </a>
-        <a className="team-link" style={{ padding: '0,20%' }} >
-          <img src="cat.png" alt="Nick Carpinito" width="300" height="300"style={{ borderRadius: '10px' }}/>
-          <div className="team-text"><h2>Nick Carpinito</h2></div>
-          <div className="team-buttons">
-            <a href="https://www.linkedin.com/in/nick-carpinito/" className="other-icon"><img src="linkedin.png" alt="LinkedIn" height="100%" /></a>
-            <a href="https://twitter.com/0xmetalight" className="other-icon"><img src="x.png" alt="X Logo" height="100%" /></a>
-          </div>
-        </a>
-      </div>
+      <Team/>
       <div><TypeformEmbed /></div>
     </div>
   )
