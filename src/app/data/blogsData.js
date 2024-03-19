53f8c5200653606e90593f945486e866d8437c8c
meta-lite <nick@myceliumnetworks.com>
2024-03-19 13:24:48 -0500

    add article

--- src/app/data/blogsData.js
+++ src/app/data/blogsData.js
@@ -3,7 +3,8 @@ const blogs = [
     { url: "https://medium.com/@meta-light/exploring-the-future-of-decentralized-physical-infrastructure-networks-the-mycelium-testbed-f8ea13ca1b4a", title: "Exploring the Future of Decentralized Physical Infrastructure Networks: The Mycelium Testbed" },
     { url: "https://medium.com/@meta-light/on-excellent-hardware-in-tech-and-its-relationship-with-depin-9c830e400c0b", title: "On Hardware Excellence in Tech, and it’s Relationship with DePIN" },
     { url: "https://medium.com/@meta-light/the-helium-ecosystem-is-evolving-34cc6c9e053d", title: "The Helium Ecosystem is Evolving 🎈" },
-    { url: "https://medium.com/@meta-light/the-ultimate-helium-resource-guide-3d29063a62a2", title: "The Ultimate Helium Resource Guide" }
+    { url: "https://medium.com/@meta-light/the-ultimate-helium-resource-guide-3d29063a62a2", title: "The Ultimate Helium Resource Guide" },
+    { url: "https://medium.com/@tylerboscolo/creating-paradigm-shifts-when-pioneers-get-shot-and-settlers-get-rich-37ab2b18279f", title: "Creating Paradigm Shifts When Pioneers Get Shot and Settlers Get Rich" }
   ];
 
   export default blogs;
