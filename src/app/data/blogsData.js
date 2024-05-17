55d7ca942b9ed46ca7fea8eaef85645da0e50792
meta-lite <nick@myceliumnetworks.com>
2024-05-17 08:06:45 -0500

    add articles

--- src/app/data/blogsData.js
+++ src/app/data/blogsData.js
@@ -4,7 +4,14 @@ const blogs = [
     { url: "https://medium.com/@meta-light/on-excellent-hardware-in-tech-and-its-relationship-with-depin-9c830e400c0b", title: "On Hardware Excellence in Tech, and it’s Relationship with DePIN" },
     { url: "https://medium.com/@meta-light/the-helium-ecosystem-is-evolving-34cc6c9e053d", title: "The Helium Ecosystem is Evolving 🎈" },
     { url: "https://medium.com/@meta-light/the-ultimate-helium-resource-guide-3d29063a62a2", title: "The Ultimate Helium Resource Guide" },
-    { url: "https://medium.com/@tylerboscolo/creating-paradigm-shifts-when-pioneers-get-shot-and-settlers-get-rich-37ab2b18279f", title: "Creating Paradigm Shifts When Pioneers Get Shot and Settlers Get Rich" }
+    { url: "https://medium.com/@tylerboscolo/creating-paradigm-shifts-when-pioneers-get-shot-and-settlers-get-rich-37ab2b18279f", title: "Creating Paradigm Shifts When Pioneers Get Shot and Settlers Get Rich" },
+    { url: "https://medium.com/@DepinDaily/what-is-mycelium-networks-c0cee3c761d6", title: "What is Mycelium Networks?" },
+    { url: "https://medium.com/@DepinDaily/silencio-network-is-community-powered-network-that-rewqards-users-for-providing-hyper-local-noise-04d6e6bfd19b", title: "Silencio Network attempts to tackle Noise Pollution. How you can contribute to this goal and earn passive income at the same time." },
+    { url: "https://medium.com/coinmonks/depin-its-an-opportunity-to-make-10-000-from-scratch-bb20dcf97714", title: "DePin — it’s an opportunity to make $10,000 from scratch." },
+    { url: "https://medium.com/coinmonks/here-are-the-best-8-depin-picks-for-you-5e965dc1c80c", title: "Here are the best 8 DePIN Picks for You" },
+    { url: "https://medium.com/@DepinDaily/why-i-think-powerpod-will-succeed-0fbcec8001fe", title: "Why I Think PowerPod Will Succeed" },
+    { url: "https://medium.com/@DepinDaily/what-is-drop-wireless-22ad8a029fbc", title: "What is Drop Wireless?" },
+    { url: "https://medium.com/@DepinDaily/what-is-depin-decentralized-physical-infrastructure-explained-4d44a50dc705", title: "What is DePIN? Decentralized Physical Infrastructure Explained" },
   ];
 
   export default blogs;
