6d8bc34dd8ad73adb7157a1563f0a6d0734dc9e9
meta-lite <nick@myceliumnetworks.com>
2024-08-01 08:29:08 -0500

    lfg

--- src/app/PopupWrapper.tsx
+++ src/app/PopupWrapper.tsx
@@ -9,12 +9,7 @@ export default function PopupWrapper() {
 
   return (
     <>
-      <Button 
-        className="px-2 py-1 w-3/5" 
-        onClick={() => setShowPopup(true)}
-      >
-        Get Notified
-      </Button>
+      <Button className="px-2 py-1 w-3/5" onClick={() => setShowPopup(true)}>Get Notified</Button>
       {showPopup && <Popup handleShowModal={() => setShowPopup(false)} />}
     </>
   )
--- src/pages/api/images.js
+++ src/pages/api/images.js
@@ -2,15 +2,13 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  // Adjust the path to your ecosystem directory accordingly
   const imagesDirectory = path.join(process.cwd(), 'public/ecosystem');
   fs.readdir(imagesDirectory, (err, files) => {
     if (err) {
       console.error('Failed to list images:', err);
       return res.status(500).json({ message: 'Failed to list images' });
     }
-    const imageUrls = files.filter(file => /\.(png|jpe?g|svg)$/.test(file))
-                           .map(file => `/ecosystem/${file}`);
+    const imageUrls = files.filter(file => /\.(png|jpe?g|svg)$/.test(file)).map(file => `/ecosystem/${file}`);
     res.status(200).json(imageUrls);
   });
 }
